ebb8e0707ed391f17e43fbae544076a3602667a8
Lada <40867844+ladabolotina@users.noreply.github.com>
2020-02-18 15:33:20 +0300

    commit

--- bislite/gulpfile.js
+++ bislite/gulpfile.js
@@ -27,15 +27,15 @@ gulp.task('browser-sync', function() {
 
 function bsReload(done) { browserSync.reload(); done(); };
 
-// gulp.task('scripts', function() {
-//     return gulp.src([
-//         'app/libs/jquery/dist/jquery.min.js',
-//         'app/libs/owl.carousel/dist/owl.carousel.min.js',
-//     ])
-//     .pipe(concat('libs.min.js'))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('app/js'));
-// });
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.js',
+        'app/libs/owl.carousel/dist/owl.carousel.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
 
 gulp.task('code', function() {
     return gulp.src('app/*.html')
